df27daa2f589320b6ee1becfed1b313723a052f6
mzlchou <mzlchou@gmail.com>
2026-04-01 00:07:47 -0400

    edit: test.sh includes util/ folder for C++ building -I../util

--- test.sh
+++ test.sh
@@ -30,7 +30,7 @@ for module in ${modules} ; do
     fi
     echo 'building'
     if [[ -f obj/tb.o ]] ; then rm obj/tb.o ; fi
-    if ! verilator -sv --cc --Wall --Wpedantic --Mdir obj -f tb/${module}/files.f --top-module ${module} --exe --build tb/${module}/tb.cpp >>${log} ; then
+    if ! verilator -sv --cc --Wall --Wpedantic --Mdir obj -CFLAGS '-I../util' -f tb/${module}/files.f --top-module ${module} --exe --build tb/${module}/tb.cpp >>${log} ; then
         echo "verilator failed to compile testbench for ${module}" 1>&2
         exit 1
     fi
